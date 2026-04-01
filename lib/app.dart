90d1fff216d400221a5e856fa4c6de13bbcd04a0
AfzalHassan <afzal.hassan@arimaclanka.com>
2026-04-01 12:25:41 +0530

    UI implemented

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:muwaqqit/features/dashboard/data/repositories/dashboard_repository_impl.dart';
+import 'package:muwaqqit/features/dashboard/presentation/cubits/dashboard_cubit.dart';
 import 'package:muwaqqit/features/dashboard/presentation/screens/Dashboard.dart';
 
 import 'core/theme/app_theme.dart';
@@ -8,11 +11,16 @@ class Muwaqqit extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Muwaqqit',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.light,
-      home: const Dashboard(),
+    return BlocProvider(
+      create: (_) => DashboardCubit(
+        repository: const DashboardRepositoryImpl(),
+      ),
+      child: MaterialApp(
+        title: 'Muwaqqit',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.light,
+        home: const Dashboard(),
+      ),
     );
   }
 }
